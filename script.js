753ac4496a8f676dd93f027f11b654a9bd03a73b
Jenny Zhang <canaantt@gmail.com>
2018-09-17 19:35:29 -0700

    .

--- script.js
+++ script.js
@@ -55,6 +55,7 @@ connection.once('open', function(){
 //#region Transformation 
 var samplemap = require('./gbmnatgen2016_samplemap.json');
 var dashboard = require('./gbmnatgen2016_dashboard.json');
+var diagnosis = require('./gbmnatgen2016_diagnosis.json');
 var network = require('./gbmnatgen2016_network.json');
 var mut01 = require('./gbmnatgen2016_somaticmutation(savi2)_mut01.json');
 var mut = require('./gbmnatgen2016_somaticmutation(savi2).json');
@@ -126,11 +127,80 @@ clinical.values = values;
 jsonfile.writeFile('output/clinical.json', clinical, function(err) {
     console.log(err);
 });
-// cnv
-var ids = Object.keys(cnv[1].data);
 
+// events
+var event_diagnosis = {};
+var data = diagnosis.map(d => {
+    var obj = _.omit(d, ['_id', 'patient_ID', 'age_at_diagnosis']);
+    var arr = [];
+    arr[0] = d['patient_ID'];
+    arr[1] = 0;
+    arr[2] = d['age_at_diagnosis'];
+    arr[3] = arr[1];
+    arr[4] = obj;
+    return arr;
+});
+event_diagnosis.map = {
+    "Diagnosis": "Status"
+};
+event_diagnosis.data = data;
+jsonfile.writeFile('output/events.json', event_diagnosis, function(err) {
+    console.log(err);
+});
+
+// cnv
+var matrix_cna = {};
+var ids = Object.keys(cnv[1].data);
+var genes = cnv.map(c => c.id);
+var data = cnv.map(c => _.values(c.data));
+
+matrix_cna.ids = ids;
+matrix_cna.genes = genes;
+matrix_cna.data = data;
+jsonfile.writeFile('output/matrix-cna.json', matrix_cna, function(err) {
+    console.log(err);
+});
 // mut
-var ids = Object.keys(mut[1].data);
+var mutation = {};
+var ids = [];
+mut.forEach(m => {
+    ids = _.uniq(_.concat(ids, Object.keys(m.data)));
+});
+
+var genes = mut.map(m => m.id);
+var muts = {
+    "splice_site": 1,
+    "silent": 2,
+    "missense_mutation": 4,
+    "intron": 8,
+    "nonsense_mutation": 16,
+    "rna": 32,
+    "3'flank": 64,
+    "5'flank": 128,
+    "in_frame_del": 256,
+    "in_frame_ins": 512,
+    "frame_shift_del": 1024,
+    "frame_shift_ins": 2048,
+    "5'utr": 4096
+};
+var values = [];
+mut.forEach(m => {
+    Object.keys(m.data).forEach(key => {
+        if(m.data[key] != ''){
+            var str = genes.indexOf(m.id) + '-' + ids.indexOf(key) + '-4';
+            values = values.concat(str);
+        }
+    });
+    
+});
+
+mutation.genes = genes;
+mutation.ids = ids;
+mutation.muts = muts;
+mutation.values = values;
+jsonfile.writeFile('output/mutation.json', mutation, function(err) {
+    console.log(err);
+});
 
 // manifest
 
